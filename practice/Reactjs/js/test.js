860383bcbe8ae0cd94b6f829af06a9b26e3215c9
Man Man <chethiminhman121998@gmail.com>
2020-08-19 15:44:44 +0700

    Updates

--- practice/Reactjs/js/test.js
+++
@@ -1,6 +0,0 @@
-const e = React.createElement;
-
-ReactDOM.render(
-  e('div', {class:"manman"}, 'Hello World'),
-  document.getElementById('root')
-);
---
+++ precache-manifest.c269b6d97bb69dc32d1e0a7a2da49e1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb3dd684f47d996731e95511105552cf",
+    "url": "/cheman121998/cheman121998.github.io/index.html"
+  },
+  {
+    "revision": "b8ee679335ab4b8bb543",
+    "url": "/cheman121998/cheman121998.github.io/static/css/main.42b2e7ab.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/cheman121998/cheman121998.github.io/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cheman121998/cheman121998.github.io/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8ee679335ab4b8bb543",
+    "url": "/cheman121998/cheman121998.github.io/static/js/main.bef56f3d.chunk.js"
+  },
+  {
+    "revision": "6d4748b9c478485f8b26",
+    "url": "/cheman121998/cheman121998.github.io/static/js/runtime-main.4574827a.js"
+  },
+  {
+    "revision": "e75d54a51d4fd4e25a2d6e9a516a06bd",
+    "url": "/cheman121998/cheman121998.github.io/static/media/bg1.e75d54a5.png"
+  },
+  {
+    "revision": "0ed31c59566beb25845a59b39a24f25b",
+    "url": "/cheman121998/cheman121998.github.io/static/media/black.0ed31c59.svg"
+  }
+]);
